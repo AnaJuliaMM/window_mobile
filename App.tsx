4e7faaeb36cefa8114d1b760021ed3a530d45a59
AnaJuliaMM <ana.martins_2004@outlook.com>
2023-12-05 21:38:15 -0300

    feat: connection to the api

--- App.tsx
+++ App.tsx
@@ -3,15 +3,44 @@ import { StyleSheet, Text, View } from 'react-native';
 import DayPrediction from './components/DayPrediction';
 import { LinearGradient } from 'expo-linear-gradient';
 import TodayPrediction from './components/TodayPrediction';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://api.exemplo.com/previsao/'); // Substitua pela URL do seu endpoint
+        setData(response.data); // Assume que os dados são um objeto JSON
+        console.log(data);
+        
+      } catch (error) {
+        console.log(error);
+        
+        console.error('Erro ao buscar dados:', error);
+      }
+    };
+
+    fetchData();
+  }, []); 
+
+
   return (
     <LinearGradient
       colors={['#608DE6', '#7FCED9']}
       style={styles.container}
     >
-      <View>
-          <TodayPrediction city_id='Sorocaba' temperature={17}/>
+      <View> 
+          {data ? ( <Text>Dados recebidos: {JSON.stringify(data)}</Text>) : (
+            <Text>Carregando dados...</Text>
+          )
+          }
+
+      
+
+          {/* // <TodayPrediction city_id='Sorocaba' temperature={17}/> */}
       </View>
     </LinearGradient>
   );

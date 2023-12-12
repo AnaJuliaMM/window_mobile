b4cdc055876ab65c8f1771fe990fb44ff094be73
AnaJuliaMM <ana.martins_2004@outlook.com>
2023-12-12 17:30:11 -0300

    chore: logic to extract data from the api

--- components/PredictionBox.tsx
+++ components/PredictionBox.tsx
@@ -1,8 +1,63 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native'
+import React, { useState, useEffect } from 'react'
 import Prediction from './Prediction'
+import { fetchData, ApiResponse } from '../utils/API'
+
+interface PredictionBoxInterface{
+    ApiResponse:  ApiResponse[]
+}
 
 export default function PredictionBox() {
+    // Código APP.tsx
+    const [apiResponse, SetApiResponse] = useState< ApiResponse[]>([]);
+    const [loading, setLoading] = useState(false);
+
+
+    const fetchDataFromApi = async () => {
+        try {
+          setLoading(true);
+          const response = await fetchData('previsao'); 
+          SetApiResponse(response);
+        } finally {
+          setLoading(false);
+        }
+    };
+
+    // Traz a previsão quando o página carrega
+    useEffect(() => {
+        fetchDataFromApi();
+    }, []);
+
+    // Retorna o símbolo de carregamento caso os dados não forem recuperados
+    if (loading) {
+        return (
+          <View style={styles.container}>
+            <ActivityIndicator size="large" color="#000" />
+          </View>
+        );
+    }
+
+
+    // Tratar o json
+    const hours = [10, 13, 18]
+    const [predictions, setPredictions] = useState <ApiResponse[] >([])
+
+    //Função de filtragem
+    const filterByHour = (ApiResponse: ApiResponse[], hour: number) => {
+        return ApiResponse.filter((prediction)=> { 
+            return hour === new Date(prediction.date).getHours();
+        })
+
+    }
+
+    // Chamar a função de comparação para cada hora
+    hours.forEach((hour)=>{
+        setPredictions(predictions.concat(filterByHour(apiResponse, hour)))
+    })
+
+
+
+
   return (
     <View style={styles.wrapper}>
         <View style={styles.header}>
@@ -12,7 +67,10 @@ export default function PredictionBox() {
                 style={{ width: 22, height: 22 }}/>
         </View>
         <View style={styles.predictions}>
-            <Prediction shift='Agora' hour={10} prediction={17}/>
+            {predictions.map((prediction)=> (
+                <Prediction shift='Novo' hour={new Date(prediction.date).getHours()} prediction={prediction.temperature} />
+            ))}
+            
             <Prediction shift='Tarde' hour={13} prediction={22}/>
             <Prediction shift='Noite' hour={18} prediction={14}/>
         </View>
@@ -21,6 +79,11 @@ export default function PredictionBox() {
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
     wrapper:{
         height: 134,
         width: 323,
---
+++ utils/API.tsx
@@ -0,0 +1,35 @@
+import axios, { AxiosResponse, AxiosError } from 'axios';
+
+const BASE_URL = 'http://sensorwindow.pythonanywhere.com/api/';
+
+export interface ApiResponse {
+  city_id: number;
+  city_name: string;
+  country: string;
+  date: string;
+  date_br: string;
+  humidity: number;
+  id: number;
+  precipitation: number;
+  pressure: number;
+  state: string;
+  temperature: number;
+  wind_direction: string;
+  wind_direction_degrees: number | null;
+  wind_gust: number;
+  wind_velocity: number;
+}
+
+export const fetchData = async (endpoint: string): Promise<ApiResponse[]> => {
+  try {
+    const response: AxiosResponse<ApiResponse[]> = await axios.get(`${BASE_URL}/${endpoint}`);
+    return response.data;
+  } catch (error) {
+    // O tipo do erro é AxiosError
+    const axiosError = error as AxiosError;
+    console.error('Erro ao consumir a API:', axiosError.message);
+    console.error('Status do erro:', axiosError.response?.status);
+    throw new Error('Falha ao obter dados da API');
+  }
+};
+ 

b7ea12fa4a5abc99a37914959900147264182a9a
camiyuka <camila.yatabe@gmail.com>
2023-12-12 16:39:22 -0300

    chore: importing other components to app

--- App.tsx
+++ App.tsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import axios from 'axios';
-import Predictions from './components/Predictions';
 import Window from './components/Window';
+import Header from './components/Header';
+import Predictions from './components/Predictions';
 
 export default function App() {
   const [data, setData] = useState< Response | null>(null);
@@ -25,12 +26,14 @@ export default function App() {
     
     <LinearGradient colors={['#608DE6', '#7FCED9']} style={styles.container}>
        
-      <View style={styles.innerContainer}>
+      <View style={styles.container}>
         {data ? (
           <View>
+            <Header/>
+            {/* colocar o componente Temperature aqui */}
             <Window is_raining={data.is_raining} />
-                <Predictions />
-    
+            <Predictions/>
+            
           </View>
         ) : (
           <Text> Carregando dados...</Text>
